ebc353c8e81bf441235b0821108043839b583c8f
Dhaval Patel <dhpatel@mobiquityinc.com>
2019-07-12 17:01:47 +0530

    change the card position

--- src/components/carddisplay.jsx
+++ src/components/carddisplay.jsx
@@ -6,21 +6,35 @@ export default class CardDisplay extends Component {
   constructor() {
     super()
     this.state = {
-      cards:[],
+      cards: [],
     }
   }
   renderCard = () => {
     const cards = this.props.cards;
-   
+    let sevenIndex = 0;
     if (!cards) return [];
-    return cards.map((item,index) => {
+    return cards.map((item, index) => {
       let cardClass
-       if(item.selected) {
-         cardClass = `cell selected ${item.rank} ${item.suite}`;
-       } else {
+      if (item.selected) {
+        cardClass = `cell selected ${item.rank} ${item.suite}`;
+      } else {
         cardClass = `cell ${item.rank} ${item.suite}`;
-       }
-    
+      }
+      if (index === 5 || sevenIndex === 7) { 
+        sevenIndex = 0;
+        sevenIndex = sevenIndex + 1;
+        return (
+          <>
+            <br />
+            <div key={item._id} className="cell" id={item.index} data-classid={item._id} onClick={this.props.onCardSelect}>
+              <div className={cardClass} />
+            </div>
+          </>
+        );
+      }
+      if(sevenIndex > 0) {
+        sevenIndex = sevenIndex + 1;
+      }
       return (
         <div key={item._id} className="cell" id={item.index} data-classid={item._id} onClick={this.props.onCardSelect}>
           <div className={cardClass} />
@@ -31,7 +45,7 @@ export default class CardDisplay extends Component {
   render() {
     return (
       <div className="card">
-          {this.renderCard()}
+        {this.renderCard()}
       </div>
     );
   }
